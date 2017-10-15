e000807fdea270f3fbb2523ea754e205f3b7f23a
DESKTOP-4AQJA74\sordc <sordcpt.blakburger1@gmail.com>
2017-10-15 03:34:53 -0400

    flask post request done, testmodel h5 committed

--- flaskEnd.py
+++ flaskEnd.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import ShowTell
 import subprocess
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -9,9 +10,9 @@ CORS(app)
 
 @app.route('/', methods=["POST", "OPTION"])
 def decision():
-    results = ShowTell.print()
-    print(results)
-    return jsonify(results)
+    list = ShowTell.print().tolist()
+    print(list)
+    return json.dumps(list)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
